d286dbfed48b30d4d6fedf1746b91da6dbf48691
Laurentiu1986 <amzarescu41@gmail.com>
2021-03-23 22:41:36 +0200

    added dialog and chips

--- src/Pages/Equipments.js
+++ src/Pages/Equipments.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+
+import PropTypes from "prop-types";
 
 import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+
+import { withStyles } from "@material-ui/core/styles";
 
 import ColorTheme from "../Theme/ThemeProvider";
 
@@ -12,6 +16,19 @@ import StepContent from "@material-ui/core/StepContent";
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Chip from "@material-ui/core/Chip";
+import Avatar from "@material-ui/core/Avatar";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+import Dialog from "@material-ui/core/Dialog";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 const useStyles = makeStyles((theme) => ({
   gridContainer: {
@@ -38,44 +55,91 @@ const useStyles = makeStyles((theme) => ({
     borderColor: ColorTheme.palette.primary.errorButton,
     fontWeight: "bold",
   },
-  actionsContainer: {
-    marginBottom: theme.spacing(2),
-  },
   resetContainer: {
     padding: theme.spacing(3),
   },
   stepIconCustom: {
-    color: ColorTheme.palette.primary.activeBorder + '!important',
+    color: ColorTheme.palette.primary.activeBorder + "!important",
   },
+  stepContent: {
+    width: "100%",
+    paddingBottom: 30,
+    fontWeight: "bold",
+  },
+  formLabelRoot: {
+    "&$formLabelFocused": {
+      color: "rgba(255, 255, 255, 0.1)",
+    },
+  },
+  labelText: {
+    fontWeight: "bold!important",
+  },
+  dialogPaper: {
+    width: '80%!important',
+    maxHeight: 435,
+  }
 }));
 
-function getSteps() {
-  return ["Select campaign settings", "Create an ad group", "Create an ad"];
-}
+const options = [
+  {
+    id: 1,
+    label: "int",
+  },
+  {
+    id: 2,
+    label: "text",
+  },
+  {
+    id: 3,
+    label: "float",
+  },
+  {
+    id: 4,
+    label: "datetime",
+  },
+];
 
-function getStepContent(step) {
-  switch (step) {
-    case 0:
-      return `For each ad campaign that you create, you can control how much
-              you're willing to spend on clicks and conversions, which networks
-              and geographical locations you want your ads to show on, and more.`;
-    case 1:
-      return "An ad group contains one or more ads which target a shared set of keywords.";
-    case 2:
-      return `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`;
-    default:
-      return "Unknown step";
-  }
-}
+const CustomTextField = withStyles({
+  root: {
+    paddingBottom: 20,
+    "& label.Mui-focused": {
+      color: "rgba(255, 255, 255, 0.1)",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "rgba(255, 255, 255, 0.1)",
+    },
+  },
+})(TextField);
 
 export const Equipments = () => {
   const classes = useStyles();
 
-  const [activeStep, setActiveStep] = React.useState(0);
+  const [activeStep, setActiveStep] = useState(0);
+  const [chipData, setChipData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = React.useState('int');
+  const [activeChip, setActiveChip] = useState(0)
+
+  const handleClose = (newValue) => {
+    setOpen(false);
+
+    if (newValue) {
+      setValue(newValue);
+      // console.log(activeChip)
+      // console.log(new_chip_data[activeChip]);
+      // new_chip_data[activeChip].type = value;
+      // setChipData(new_chip_data);
+    }
+  };
+
   const steps = getSteps();
+
+  React.useEffect(() => {
+    console.log(chipData);
+    // let new_chip_data = [...chipData];
+    // console.log(new_chip_data[activeChip]);
+
+  }, [chipData]);
 
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -87,6 +151,165 @@ export const Equipments = () => {
 
   const handleReset = () => {
     setActiveStep(0);
+  };
+
+  const handleDelete = (chipToDelete) => () => {
+    setChipData((chips) =>
+      chips.filter((chip) => chip.key !== chipToDelete.key)
+    );
+  };
+
+  function getSteps() {
+    return [
+      "Set the name of the equipment",
+      "Add the equipment's features",
+      "Select a type for every feature",
+    ];
+  }
+
+  function keyPress(e) {
+    if (e.keyCode === 13) {
+      console.log(e.target.value);
+      setChipData((chipData) => [
+        ...chipData,
+        { key: chipData.length + 1, label: e.target.value, type: null },
+      ]);
+    }
+  }
+
+  function ConfirmationDialogRaw(props) {
+    const { onClose, value: valueProp, open, ...other } = props;
+    const [value, setValue] = React.useState(valueProp);
+
+    const radioGroupRef = React.useRef(null);
+
+    React.useEffect(() => {
+      if (!open) {
+        setValue(valueProp);
+      }
+    }, [valueProp, open]);
+
+    const handleEntering = () => {
+      if (radioGroupRef.current != null) {
+        radioGroupRef.current.focus();
+      }
+    };
+
+    const handleCancel = () => {
+      onClose();
+    };
+
+    const handleOk = () => {
+      onClose(value);
+    };
+
+    const handleChange = (event) => {
+      setValue(event.target.value);
+    };
+
+    return (
+      <Dialog
+        disableBackdropClick
+        disableEscapeKeyDown
+        maxWidth="xs"
+        onEntering={handleEntering}
+        aria-labelledby="confirmation-dialog-title"
+        open={open}
+        {...other}
+      >
+        <DialogTitle id="confirmation-dialog-title">Phone Ringtone</DialogTitle>
+        <DialogContent dividers>
+          <RadioGroup
+            ref={radioGroupRef}
+            aria-label="ringtone"
+            name="ringtone"
+            value={value}
+            onChange={handleChange}
+          >
+            {options.map((option) => (
+              <FormControlLabel
+                value={option.label}
+                key={option.id}
+                control={<Radio />}
+                label={option.label}
+              />
+            ))}
+          </RadioGroup>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleCancel} color="primary" className={classes.buttonReset}>
+            Cancel
+          </Button>
+          <Button onClick={handleOk} color="primary" className={classes.buttonNext}>
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+
+  function getStepContent(step) {
+    switch (step) {
+      case 0:
+        return (
+          <Grid item xs={12}>
+            <form noValidate autoComplete="off">
+              <CustomTextField id="standard-basic" label="Standard" />
+            </form>
+          </Grid>
+        );
+      case 1:
+        return (
+          <Grid item xs={12}>
+            <CustomTextField
+              id="standard-basic"
+              label="Standard"
+              onKeyDown={keyPress}
+            />
+            <br></br>
+            {chipData.map((data) => {
+              return (
+                <Chip
+                  style={{ margin: "0 10px 10px 0" }}
+                  key={data.key}
+                  avatar={<Avatar>{data.label[0]}</Avatar>}
+                  label={data.label}
+                  onClick={handleClick(data)}
+                  onDelete={handleDelete(data)}
+                />
+              );
+            })}
+            <ConfirmationDialogRaw
+              classes={{
+                paper: classes.paper,
+              }}
+              id="ringtone-menu"
+              keepMounted
+              open={open}
+              onClose={handleClose}
+              value={value}
+            />
+          </Grid>
+        );
+      case 2:
+        return `Try out different ad text to see what brings in the most customers,
+                and learn how to enhance your ads using features like ad extensions.
+                If you run into any problems with your ads, find out how to tell if
+                they're running and how to resolve approval issues.`;
+      default:
+        return "Unknown step";
+    }
+  }
+
+  ConfirmationDialogRaw.propTypes = {
+    onClose: PropTypes.func.isRequired,
+    open: PropTypes.bool.isRequired,
+    value: PropTypes.string.isRequired,
+  };
+
+  const handleClick = (chip) => () => {
+    setOpen(true);
+    setActiveChip(chip.key)
   };
 
   return (
@@ -104,6 +327,9 @@ export const Equipments = () => {
             {steps.map((label, index) => (
               <Step key={label}>
                 <StepLabel
+                  classes={{
+                    label: classes.labelText,
+                  }}
                   StepIconProps={{
                     classes: {
                       completed: classes.stepIconCustom,
@@ -115,7 +341,9 @@ export const Equipments = () => {
                   {label}
                 </StepLabel>
                 <StepContent>
-                  <Typography>{getStepContent(index)}</Typography>
+                  <Grid container className={classes.stepContent}>
+                    {getStepContent(index)}
+                  </Grid>
                   <div className={classes.actionsContainer}>
                     <div>
                       <Button
--- src/Theme/ThemeProvider.js
+++ src/Theme/ThemeProvider.js
@@ -22,6 +22,7 @@ const ColorTheme = createMuiTheme({
   typography: {
     fontFamily: "Montserrat, sans-serif",
   },
+  spacing: 8
 });
 
 export default ColorTheme;
